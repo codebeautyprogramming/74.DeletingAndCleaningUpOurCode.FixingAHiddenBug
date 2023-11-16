f789aed65e3293c7c1a115940e31b34501795027
codebeautyprogramming <codebeautyprogramming@gmail.com>
2023-11-16 02:08:05 +0100

    Startup code for this chapter

---
+++ CookBook/Program.cs
@@ -0,0 +1,43 @@
+using CookBook.UI;
+using DataAccessLayer.Contracts;
+using DataAccessLayer.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Configuration;
+
+namespace CookBook
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+
+            ServiceCollection services = ConfigureServices();
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            var startForm = serviceProvider.GetRequiredService<IngredientsForm>();
+            Application.Run(startForm);
+        }
+
+        static ServiceCollection ConfigureServices()
+        {
+            ServiceCollection services = new ServiceCollection();
+
+            if (ConfigurationManager.AppSettings["repositoryType"] == "txt")
+                services.AddTransient<IIngredientsRepository>(_ => new IngredientsTxtRepository());
+            else
+                services.AddTransient<IIngredientsRepository>(_ => new IngredientsRepository());
+
+            services.AddTransient<IngredientsForm>();
+
+            return services;
+
+        }
+
+    }
+}
---
+++ DataAccessLayer/Repositories/IngredientsTxtRepository.cs
@@ -0,0 +1,53 @@
+﻿using DataAccessLayer.Contracts;
+using DomainModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class IngredientsTxtRepository : IIngredientsRepository
+    {
+        string _filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "IngredientsStorage.txt");
+        public void AddIngredient(Ingredient ingredient)
+        {
+            int id = Math.Abs(Guid.NewGuid().GetHashCode());
+
+            using (StreamWriter sw = File.AppendText(_filePath))
+            {
+                sw.WriteLine($"{id}|{ingredient.Name}|{ingredient.Weight}|{ingredient.KcalPer100g}|{ingredient.PricePer100g}|{ingredient.Type}");
+            }  
+        }
+
+        public List<Ingredient> GetIngredients(string? name = "")
+        {
+            List<Ingredient> ingredients = new List<Ingredient>();
+
+            using(StreamReader sr = File.OpenText(_filePath)) 
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    string[] values = line.Split('|');
+
+                    Ingredient ingredient = new Ingredient();
+                    ingredient.Id = int.Parse(values[0]);
+                    ingredient.Name = values[1];
+                    ingredient.Weight = decimal.Parse(values[2]);
+                    ingredient.KcalPer100g = decimal.Parse(values[3]);
+                    ingredient.PricePer100g = decimal.Parse(values[4]);
+                    ingredient.Type = values[5];
+
+                    if(string.IsNullOrEmpty(name))
+                        ingredients.Add(ingredient);
+                    else if (!string.IsNullOrEmpty(name) && ingredient.Name.ToLower().StartsWith(name.ToLower()))
+                        ingredients.Add(ingredient);
+
+                }
+            }
+            return ingredients;
+        }
+    }
+}
